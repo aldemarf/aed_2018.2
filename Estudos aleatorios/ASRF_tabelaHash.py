e8f95029dc77d894be7f87c7f76fb73b1f29200d
Aldemar F <aldemar.poli@gmail.com>
2018-12-20 08:31:26 -0300

    functional hash

--- Estudos aleatorios/ASRF_tabelaHash.py
+++ Estudos aleatorios/ASRF_tabelaHash.py
@@ -4,6 +4,10 @@
 # (Utilizado listas concretas do python)
 
 from math import ceil, fmod, sqrt
+
+class MinimumError(Exception):
+	pass
+
 class HashChainUnity:
 	def __init__(self, key, value):
 		self.__key = key
@@ -36,12 +40,15 @@ class HashChainUnity:
 		self.__value = newValue
 
 	@property
-	def next(self):
+	def nextItem(self):
 		return self.__next
 
-	@next.setter
-	def next(self, newValue):
+	@nextItem.setter
+	def nextItem(self, newValue):
 		self.__next = newValue
+
+	def next(self):
+		return self.__next
 
 
 class HashTable:
@@ -53,18 +60,21 @@ class HashTable:
 		self.__size = slots
 		self.__table = [None for a in range(slots)]
 
+		if slots < self.PRIME:
+			raise MinimumError("Value don't reached the minimum necessary!")
+
 
 	def __9Hash__(self, key):
 		address = fmod(key, 9)
-		return address
+		return int(address)
 
 	def __divisionHash__(self, key):
 		address = fmod(key, self.PRIME)
-		return address
+		return int(address)
 
 	def __MultiplyHash__(self, key):
 		address = ceil(self.PRIME * fmod(key * self.CONST, 1))
-		return address
+		return int(address)
 
 
 	@property
@@ -76,18 +86,31 @@ class HashTable:
 		return self.__table
 
 	@table.setter
-	def table(self, hashValue, item):
+	def table(self, entry):
+		hashValue, item = entry
 		self.__table[hashValue] = item
 
-	def sweepBucket(self, bucket, key):
-		pass
+	def sweepBucket(self, item, key):
+		while item:
+			if key == item.key:
+				return item.pair
+			item = item.next()
+		return None
+
+	def get(self, key):
+		hashKey = self.__9Hash__(key)
+		bucket = self.table[hashKey]
+		return self.sweepBucket(bucket, key)
 
 
-	def insertValue(self, key, value):
+	def insert(self, key, value):
 		item = HashChainUnity(key, value)
 		self.table = (self.__9Hash__(key), item)
 
+hashTab = HashTable(60)
+hashTab.insert(123, "ABC")
 
+print(hashTab.get(1234189))
 
 
 ################################################################
@@ -95,175 +118,175 @@ class HashTable:
 ################################################################
 
 
-#########################
-# CHAINED LINKED LIST
+# #########################
+# # CHAINED LINKED LIST
 
-class HashEntry:
-	def __init__(self, key, value):
-		self.key = key
-		self.value = value
-		self.next = None
-
-
-class HashTable:
-	def __init__(self, size):
-		self.size = size
-		self.table = [None] * self.size
-
-	def hashing_function(self, key):
-		return hash(key) % self.size
-
-	def rehash(self, entry, key, value):
-		while entry and entry.key != key:
-			prev, entry = entry, entry.next
-		if entry:
-			entry.value = value
-		else:
-			prev.next = HashEntry(key, value)
-
-	def set(self, key, value):
-		slot = self.hashing_function(key)
-		entry = self.table[slot]
-		if not entry:
-			self.table[slot] = HashEntry(key, value)
-		else:
-			self.rehash(entry, key, value)
-
-	def get(self, key):
-		hash = self.hashing_function(key)
-		if not self.table[hash]: raise KeyError
-		else:
-			entry = self.table[hash]
-			while entry and entry.key != key: entry = entry.next
-			return entry.value
+# class HashEntry:
+# 	def __init__(self, key, value):
+# 		self.key = key
+# 		self.value = value
+# 		self.next = None
 
 
+# class HashTable:
+# 	def __init__(self, size):
+# 		self.size = size
+# 		self.table = [None] * self.size
 
-#########################
-# LINEAR PROBING -- SPT
+# 	def hashing_function(self, key):
+# 		return hash(key) % self.size
 
-class HashTable:
-	def __init__(self, size):
-		self.size = size
-		self.keys = [None] * self.size
-		self.values = [None] * self.size
+# 	def rehash(self, entry, key, value):
+# 		while entry and entry.key != key:
+# 			prev, entry = entry, entry.next
+# 		if entry:
+# 			entry.value = value
+# 		else:
+# 			prev.next = HashEntry(key, value)
 
-	def hash_function(self, key):
-		return hash(key) % self.size
+# 	def set(self, key, value):
+# 		slot = self.hashing_function(key)
+# 		entry = self.table[slot]
+# 		if not entry:
+# 			self.table[slot] = HashEntry(key, value)
+# 		else:
+# 			self.rehash(entry, key, value)
 
-	def get_slot(self, key):
-		slot = self.hash_function(key)
-		while self.keys[slot] and self.keys[slot] != key:
-			slot = self.hash_function(slot + 1)
-		return slot
-
-	def set(self, key, value):
-		slot = self.get_slot(key)
-		self.keys[slot] = key
-		self.values[slot] = value
-
-	def get(self, key):
-		return self.values[self.get_slot(key)]
+# 	def get(self, key):
+# 		hash = self.hashing_function(key)
+# 		if not self.table[hash]: raise KeyError
+# 		else:
+# 			entry = self.table[hash]
+# 			while entry and entry.key != key: entry = entry.next
+# 			return entry.value
 
 
 
-class HashTable:
+# #########################
+# # LINEAR PROBING -- SPT
 
-	ratio_expand = .8
-	ratio_shrink = .2
-	min_size = 11
+# class HashTable:
+# 	def __init__(self, size):
+# 		self.size = size
+# 		self.keys = [None] * self.size
+# 		self.values = [None] * self.size
 
-	def __init__(self, size=None):
-		self._size = size or self.min_size
-		self._buckets = [None] * self._size
-		self._list = None
-		self._count = 0
+# 	def hash_function(self, key):
+# 		return hash(key) % self.size
 
-	def _entry(self, key):
-		# get hash and index
-		idx = hash(key) % self._size
-		# find entry by key
-		p, q = self._buckets[idx], None
-		while p and p.key != key:
-			p, q = p.next, p
-		# index, entry, previous entry
-		return idx, p, q
-	def _ensure_capacity(self):
-		fill = self._count / self._size
+# 	def get_slot(self, key):
+# 		slot = self.hash_function(key)
+# 		while self.keys[slot] and self.keys[slot] != key:
+# 			slot = self.hash_function(slot + 1)
+# 		return slot
 
-		# expand or shrink?
-		if fill > self.ratio_expand:
-			self._size = self._size * 2 + 1
-		elif fill < self.ratio_shrink and \
-				self._size > self.min_size:
-			self._size = (self._size - 1) // 2
-		else:
-			return
-		# reallocate buckets
-		self._buckets = [None] * self._size
-		# store entries into new buckets
-		p = self._list
-		while p:
-			idx = hash(p.key) % self._size
-			p.next = self._buckets[idx]
-			self._buckets[idx] = p
-			p = p.entry_next
-	def __len__(self):
-		return self._count
-	def __contains__(self, key):
-		_, p, _ = self._entry(key)
-		return bool(p)
-	def __getitem__(self, key):
-		_, p, _ = self._entry(key)
-		return p and p.value
-	def __setitem__(self, key, value):
-		idx, p, _ = self._entry(key)
-		# set entry if key was found
-		if p:
-			p.value = value
-			return
-		# create new entry
-		p = SimpleNamespace(
-			key=key,
-			value=value,
-			next=self._buckets[idx],
-			entry_next=self._list,
-			entry_prev=None
-		)
-		# store to bucket
-		self._buckets[idx] = p
-		# store to list
-		if self._list:
-			self._list.entry_prev = p
-		self._list = p
-		# expand
-		self._count += 1
-		self._ensure_capacity()
-	def __delitem__(self, key):
-		idx, p, q = self._entry(key)
-		# key not found
-		if not p:
-			return
-		# remove from bucket
-		if q:
-			q.next = p.next
-		else:
-			self._buckets[idx] = p.next
-		# remove from list
-		if p.entry_next:
-			p.entry_next.entry_prev = p.entry_prev
-		if p.entry_prev:
-			p.entry_prev.entry_next = p.entry_next
-		else:
-			self._list = p.entry_next
-		# shrink
-		self._count -= 1
-		self._ensure_capacity()
-	def __iter__(self):
-		p = self._list
-		while p:
-			yield p.key
-			p = p.entry_next
+# 	def set(self, key, value):
+# 		slot = self.get_slot(key)
+# 		self.keys[slot] = key
+# 		self.values[slot] = value
 
-	def slots(self):
-		return ''.join(p and 'x' or '-' for p in self._buckets)
+# 	def get(self, key):
+# 		return self.values[self.get_slot(key)]
+
+
+
+# class HashTable:
+
+# 	ratio_expand = .8
+# 	ratio_shrink = .2
+# 	min_size = 11
+
+# 	def __init__(self, size=None):
+# 		self._size = size or self.min_size
+# 		self._buckets = [None] * self._size
+# 		self._list = None
+# 		self._count = 0
+
+# 	def _entry(self, key):
+# 		# get hash and index
+# 		idx = hash(key) % self._size
+# 		# find entry by key
+# 		p, q = self._buckets[idx], None
+# 		while p and p.key != key:
+# 			p, q = p.next, p
+# 		# index, entry, previous entry
+# 		return idx, p, q
+# 	def _ensure_capacity(self):
+# 		fill = self._count / self._size
+
+# 		# expand or shrink?
+# 		if fill > self.ratio_expand:
+# 			self._size = self._size * 2 + 1
+# 		elif fill < self.ratio_shrink and \
+# 				self._size > self.min_size:
+# 			self._size = (self._size - 1) // 2
+# 		else:
+# 			return
+# 		# reallocate buckets
+# 		self._buckets = [None] * self._size
+# 		# store entries into new buckets
+# 		p = self._list
+# 		while p:
+# 			idx = hash(p.key) % self._size
+# 			p.next = self._buckets[idx]
+# 			self._buckets[idx] = p
+# 			p = p.entry_next
+# 	def __len__(self):
+# 		return self._count
+# 	def __contains__(self, key):
+# 		_, p, _ = self._entry(key)
+# 		return bool(p)
+# 	def __getitem__(self, key):
+# 		_, p, _ = self._entry(key)
+# 		return p and p.value
+# 	def __setitem__(self, key, value):
+# 		idx, p, _ = self._entry(key)
+# 		# set entry if key was found
+# 		if p:
+# 			p.value = value
+# 			return
+# 		# create new entry
+# 		p = SimpleNamespace(
+# 			key=key,
+# 			value=value,
+# 			next=self._buckets[idx],
+# 			entry_next=self._list,
+# 			entry_prev=None
+# 		)
+# 		# store to bucket
+# 		self._buckets[idx] = p
+# 		# store to list
+# 		if self._list:
+# 			self._list.entry_prev = p
+# 		self._list = p
+# 		# expand
+# 		self._count += 1
+# 		self._ensure_capacity()
+# 	def __delitem__(self, key):
+# 		idx, p, q = self._entry(key)
+# 		# key not found
+# 		if not p:
+# 			return
+# 		# remove from bucket
+# 		if q:
+# 			q.next = p.next
+# 		else:
+# 			self._buckets[idx] = p.next
+# 		# remove from list
+# 		if p.entry_next:
+# 			p.entry_next.entry_prev = p.entry_prev
+# 		if p.entry_prev:
+# 			p.entry_prev.entry_next = p.entry_next
+# 		else:
+# 			self._list = p.entry_next
+# 		# shrink
+# 		self._count -= 1
+# 		self._ensure_capacity()
+# 	def __iter__(self):
+# 		p = self._list
+# 		while p:
+# 			yield p.key
+# 			p = p.entry_next
+
+# 	def slots(self):
+# 		return ''.join(p and 'x' or '-' for p in self._buckets)
